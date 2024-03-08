aa419db707f70a119be457e9eab41907718341de
Jadon Calvert <calvertjadon@gmail.com>
2024-03-08 18:23:22 -0500

    add kanagawa

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      theme = "wave",
+      colors = {
+        theme = {
+          wave = {
+            ui = {
+              bg = "#111111",
+              bg_gutter = "#171717",
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+
+      colorscheme = "kanagawa",
+    },
+  },
+}
